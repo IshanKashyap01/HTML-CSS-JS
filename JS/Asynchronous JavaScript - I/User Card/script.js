f2e6cf95f90c0a4fa738779419977800840520d1
Ishan Kashyap <83977077+IshanKashyap01@users.noreply.github.com>
2025-02-12 21:47:42 +0530

    Addendum: `user` parameter added to `loadUser()`
    Docs: documentation comments added to the code

--- JS/Asynchronous JavaScript - I/User Card/script.js
+++ JS/Asynchronous JavaScript - I/User Card/script.js
@@ -1,17 +1,27 @@
+// HTML container element that contains all the user cards
 const cardContainer = document.querySelector('.card-container')
+// XMLHTTPRequest to get data from the server
 const request = new XMLHttpRequest()
-
 request.open('GET', 'https://dummyjson.com/users')
 request.responseType = 'json'
 request.send()
-request.addEventListener('load', loadUser)
+request.addEventListener('load', () => loadUsers(request.response.users))
 
-function loadUser()
+/**
+ * Renders user profile cards
+ * @param {Object[]} users Array of users
+ */
+function loadUsers(users)
 {
-    const users = this.response.users.map(user => createUser(user))
-    cardContainer.append(...users)
+    const userElements = users.map(user => createUser(user))
+    cardContainer.append(...userElements)
 }
 
+/**
+ * Creates a user profile card
+ * @param {Object} user an object representing a user
+ * @returns HTML element representing a user profile card
+ */
 function createUser(user)
 {
     const userCard = createElement('div', '', 'user-card')
@@ -32,6 +42,13 @@ function createUser(user)
     return userCard
 }
 
+/**
+ * Creates an HTML element of the given type, with the given text and class(es)
+ * @param {String} tag name of the element
+ * @param {String} textContent text content of the element
+ * @param {String} classlist space-separated class list
+ * @returns HTML element
+ */
 function createElement(tag, textContent, classlist = '')
 {
     const element = document.createElement(tag)
