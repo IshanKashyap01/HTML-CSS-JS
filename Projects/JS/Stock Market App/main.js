a4e4fcbeb774ac0c3eabc60ee7dc6dac2f4fd63c
Ishan Kashyap <83977077+IshanKashyap01@users.noreply.github.com>
2025-03-06 21:44:03 +0530

    Add: Controllers and main function
    Controllers for each view and a main function to initialize the page

--- Projects/JS/Stock Market App/main.js
+++ Projects/JS/Stock Market App/main.js
@@ -1,13 +1,26 @@
-/**
- * <div class="stock-option">
- *       <button class="stock">AAPL</button>
- *       <span>$3000</span>
- *       <span class="profit">2%</span>
- *   </div>
- */
+import { StockDataLoader } from './modules/services/StockDataLoader.js'
+import { ChartView } from './modules/views/ChartView.js'
+import { StockOptionsView } from './modules/views/StockOptionsView.js'
+import { StockSummaryView } from './modules/views/StockSummaryView.js'
+import { ChartController } from './modules/controllers/ChartController.js'
+import { StockOptionsController } from './modules/controllers/StockOptionsController.js'
+import { StockSummaryController } from './modules/controllers/StockSummaryController.js'
+
 main()
 
-function main()
+async function main()
 {
-
+    const loader = new StockDataLoader()
+    await Promise.all([
+        loader.loadBookValueAndProfits(),
+        loader.loadStockSummaries(),
+        loader.loadStockCharts()
+    ])
+    const stocks = loader.stocks
+    const stock = stocks.get('AAPL')
+    const optionsController = new StockOptionsController(
+        stocks, new StockOptionsView(), new StockSummaryController(new StockSummaryView()),
+        new ChartController(new ChartView(), stock, '1mo')
+    )
+    optionsController.stock = stock
 }
